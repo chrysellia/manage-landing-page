088eabb5b547b9e2b1814695740ddba17135c11a
Chrysellia <priscilliachryso@gmail.com>
2021-12-15 15:14:38 +0300

    PreFooter component created

--- website/src/MainComponent/MainComponent.component.js
+++ website/src/MainComponent/MainComponent.component.js
@@ -3,6 +3,7 @@ import Navbar from '../Navbar/Navbar.component'
 import Hero from '../Hero/Hero.component'
 import Body from '../Body/Body.component'
 import Testimonials from '../Testimonials/Testimonials.component'
+import PreFooter from '../PreFooter/PreFooter.component'
 
 const MainComponent = () => {
     return (
@@ -11,6 +12,7 @@ const MainComponent = () => {
             <Hero/>
             <Body/>
             <Testimonials/>
+            <PreFooter/>
 
         </div>
     )
---
+++ website/src/PreFooter/PreFooter.component.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './PreFooter.style.scss'
+
+const PreFooter = () => {
+    return (
+        <div className="prefooter-page">
+            <div className="prefooter-wrapper">
+                <div className="prefooter-title">Simplify how your team <br/> works today.</div>
+                <div className="prefooter-button">
+                    <div className="prefooter-style">
+                        <div className="prefooter-text">Get started</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default PreFooter
