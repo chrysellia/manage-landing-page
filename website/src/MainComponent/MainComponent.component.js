731364219a5fe25e17b230ce4cf375c1585afd3b
Chrysellia <priscilliachryso@gmail.com>
2021-12-16 14:30:38 +0300

    Body and PreFooter updated

--- website/src/MainComponent/MainComponent.component.js
+++ website/src/MainComponent/MainComponent.component.js
@@ -5,16 +5,24 @@ import Body from '../Body/Body.component'
 import Testimonials from '../Testimonials/Testimonials.component'
 import PreFooter from '../PreFooter/PreFooter.component'
 import Footer from '../Footer/Footer.component'
+import './MainComponent.style.scss'
 
 const MainComponent = () => {
     return (
-        <div>
-            <Navbar/>
-            <Hero/>
-            <Body/>
-            <Testimonials/>
-            <PreFooter/>
-            <Footer/>
+        <div className="main-wrapper">
+            <div className="main-background">
+                <div className="background-vector">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="814" height="814"><rect width="436" height="970" x="774" y="-62" fill="#FFF0EC" fill-rule="evenodd" rx="218" transform="rotate(45 718.814 -291.157)"/></svg>
+                </div>
+            </div>
+            <div className="main-component">
+                <Navbar/>
+                <Hero/>
+                <Body/>
+                <Testimonials/>
+                <PreFooter/>
+                <Footer/>
+            </div>
         </div>
     )
 }
--- website/src/PreFooter/PreFooter.component.js
+++ website/src/PreFooter/PreFooter.component.js
@@ -6,6 +6,9 @@ const PreFooter = () => {
         <div className="prefooter-page">
             <div className="prefooter-wrapper">
                 <div className="prefooter-title">Simplify how your team <br/> works today.</div>
+                <div className="prefooter-background">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="1439" height="360"><g fill="#FFF0EC" fill-rule="evenodd" opacity=".071" transform="translate(-171 -373)"><rect width="310.431" height="708" x="205" y="410" rx="155.215" transform="scale(1 -1) rotate(-45 -1484.244 0)"/><rect width="310.431" height="708" x="1269" y="7" rx="155.215" transform="scale(1 -1) rotate(-45 552.684 0)"/></g></svg>
+                </div>
                 <div className="prefooter-button">
                     <div className="prefooter-style">
                         <div className="prefooter-text">Get started</div>
