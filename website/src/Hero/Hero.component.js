c96c2daca8f9ea2dba0c9031f8ead48f96eadc1a
Chrysellia <priscilliachryso@gmail.com>
2021-12-17 13:59:42 +0300

    Hero mobile version updated

--- website/src/Hero/Hero.component.js
+++ website/src/Hero/Hero.component.js
@@ -5,31 +5,64 @@ const Hero = () => {
     return (
         <div className="hero-page">
             <div className="hero-wrapper">
-                <div className="hero-content">
-                    <div className="hero-title">Bring everyone <br/> together to build <br/> better products.</div>
 
-                    <div className="hero-description">
-                        <p>
-                            Manage makes it simple for software teams<br/>
-                            to plan day-to-day tasks while keeping the <br/>
-                            larger team goals in view.
-                        </p>
+                {/* Desktop */}
+                <div className="hero-desktop">
+                    <div className="hero-content">
+                        <div className="hero-title">Bring everyone <br/> together to build <br/> better products.</div>
+
+                        <div className="hero-description">
+                            <p>
+                                Manage makes it simple for software teams<br/>
+                                to plan day-to-day tasks while keeping the <br/>
+                                larger team goals in view.
+                            </p>
+                        </div>
+
+                        <div className="click-button">
+                            <div className="button-design">
+                                <div className="button-content">Get started</div>
+                            </div>
+                        </div>
                     </div>
 
-                    <div className="click-button">
-                        <div className="button-design">
-                            <div className="button-content">Get started</div>
+                    <div className="hero-bloc">
+                        <div className="bloc-wrapper">
+                            <div className="bloc-image">
+                                <img src="images/illustration-intro.svg"/>
+                            </div>
                         </div>
+
                     </div>
                 </div>
 
-                <div className="hero-bloc">
-                    <div className="bloc-wrapper">
-                        <div className="bloc-image">
-                            <img src="images/illustration-intro.svg"/>
+                {/* Mobile */}
+                <div className="hero-mobile">
+                    <div className="hero-bloc">
+                        <div className="bloc-wrapper">
+                            <div className="bloc-image">
+                                <img src="images/illustration-intro.svg"/>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div className="hero-content">
+                        <div className="hero-title">Bring everyone <br/> together to build <br/> better products.</div>
+
+                        <div className="hero-description">
+                            <p>
+                                Manage makes it simple for software teams<br/>
+                                to plan day-to-day tasks while keeping the <br/>
+                                larger team goals in view.
+                            </p>
+                        </div>
+
+                        <div className="click-button">
+                            <div className="button-design">
+                                <div className="button-content">Get started</div>
+                            </div>
                         </div>
                     </div>
-
                 </div>
             </div>
         </div>
